f425feaa5b5ee54d1fa180be43ee616d324446d2
tomeroron <116726289+tomeroron@users.noreply.github.com>
2023-09-04 15:27:36 +0300

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,26 @@
+def compare_text_files(file1_path, file2_path):
+    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+        lines1 = file1.readlines()
+        lines2 = file2.readlines()
+
+    if len(lines1) != len(lines2):
+        return False
+
+    for line1, line2 in zip(lines1, lines2):
+        if line1 != line2:
+            return False
+
+    return True
+
+
+
+if __name__ == "__main__":
+
+    for target in["iris", "spheres","drive1", "drive2"]:
+        for goal in ["sym", "norm", "ddg", "symnmf"]:
+            path1 = f"{goal}_{target}.txt"
+            path2 = f"{goal}_{target}_res.txt"
+            print(f"{target} {goal}: {compare_text_files(path1, path2)}")
+            if(goal!="symnmf"):
+                path2 = f"{goal}c_{target}_res.txt"
+                print(f"{target} {goal}: {compare_text_files(path1, path2)}")
---
+++ run_symnmf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+python3 symnmf.py 4 sym spheres.txt > sym_spheres_res.txt
+python3 symnmf.py 4 ddg spheres.txt > ddg_spheres_res.txt
+python3 symnmf.py 4 norm spheres.txt > norm_spheres_res.txt
+python3 symnmf.py 4 symnmf spheres.txt > symnmf_spheres_res.txt
+python3 symnmf.py 3 sym iris.txt > sym_iris_res.txt
+python3 symnmf.py 3 ddg iris.txt > ddg_iris_res.txt
+python3 symnmf.py 3 norm iris.txt > norm_iris_res.txt
+python3 symnmf.py 3 symnmf iris.txt > symnmf_iris_res.txt
+python3 symnmf.py 3 sym drive1.txt > sym_drive1_res.txt
+python3 symnmf.py 3 ddg drive1.txt > ddg_drive1_res.txt
+python3 symnmf.py 3 norm drive1.txt > norm_drive1_res.txt
+python3 symnmf.py 3 symnmf drive1.txt > symnmf_drive1_res.txt
+python3 symnmf.py 3 sym drive2.txt > sym_drive2_res.txt
+python3 symnmf.py 3 ddg drive2.txt > ddg_drive2_res.txt
+python3 symnmf.py 3 norm drive2.txt > norm_drive2_res.txt
+python3 symnmf.py 3 symnmf drive2.txt > symnmf_drive2_res.txt
+
+./symnmf sym spheres.txt > symc_spheres_res.txt
+./symnmf ddg spheres.txt > ddgc_spheres_res.txt
+./symnmf norm spheres.txt > normc_spheres_res.txt
+./symnmf sym iris.txt > symc_iris_res.txt
+./symnmf ddg iris.txt > ddgc_iris_res.txt
+./symnmf norm iris.txt > normc_iris_res.txt
+./symnmf sym drive1.txt > symc_drive1_res.txt
+./symnmf ddg drive1.txt > ddgc_drive1_res.txt
+./symnmf norm drive1.txt > normc_drive1_res.txt
+./symnmf sym drive2.txt > symc_drive2_res.txt
+./symnmf ddg drive2.txt > ddgc_drive2_res.txt
+./symnmf norm drive2.txt > normc_drive2_res.txt
+
+
+python3 compare.py
