3f8d9bf32548248b688bb3f0172232720a9cbd8d
tomeroron <116726289+tomeroron@users.noreply.github.com>
2023-09-18 15:37:05 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-rm spheres.txt  sym_spheres_res.txt ddg_spheres_res.txt     norm_spheres_res.txt symnmf_spheres_res.txt
-rm iris.txt     sym_iris_res.txt    ddg_iris_res.txt        norm_iris_res.txt    symnmf_iris_res.txt
-rm drive1.txt   sym_drive1_res.txt  ddg_drive1_res.txt      norm_drive1_res.txt  symnmf_drive1_res.txt
-rm drive2.txt   sym_drive2_res.txt  ddg_drive2_res.txt      norm_drive2_res.txt  symnmf_drive2_res.txt
-rm symc_spheres_res.txt ddgc_spheres_res.txt normc_spheres_res.txt
-rm symc_iris_res.txt ddgc_iris_res.txt normc_iris_res.txt
-rm symc_drive1_res.txt ddgc_drive1_res.txt normc_drive1_res.txt
-rm symc_drive2_res.txt ddgc_drive2_res.txt normc_drive2_res.txt
-rm compare.py
-rm run_symnmf.sh how_to_run.txt
-rm sym_spheres.txt ddg_spheres.txt      norm_spheres.txt symnmf_spheres.txt
-rm sym_iris.txt ddg_iris.txt            norm_iris.txt   symnmf_iris.txt
-rm sym_drive1.txt ddg_drive1.txt        norm_drive1.txt symnmf_drive1.txt
-rm sym_drive2.txt ddg_drive2.txt      norm_drive2.txt symnmf_drive2.txt
+rm spheres.txt  sym_spheres_res.txt ddg_spheres_res.txt     norm_spheres_res.txt    symnmf_spheres_res.txt
+rm iris.txt     sym_iris_res.txt    ddg_iris_res.txt        norm_iris_res.txt       symnmf_iris_res.txt
+rm drive1.txt   sym_drive1_res.txt  ddg_drive1_res.txt      norm_drive1_res.txt     symnmf_drive1_res.txt
+rm drive2.txt   sym_drive2_res.txt  ddg_drive2_res.txt      norm_drive2_res.txt     symnmf_drive2_res.txt
+rm symc_spheres_res.txt             ddgc_spheres_res.txt    normc_spheres_res.txt
+rm symc_iris_res.txt                ddgc_iris_res.txt       normc_iris_res.txt
+rm symc_drive1_res.txt              ddgc_drive1_res.txt     normc_drive1_res.txt
+rm symc_drive2_res.txt              ddgc_drive2_res.txt     normc_drive2_res.txt
+rm run_symnmf.sh                    how_to_run.txt          compare.py
+rm sym_spheres.txt                  ddg_spheres.txt         norm_spheres.txt        symnmf_spheres.txt
+rm sym_iris.txt                     ddg_iris.txt            norm_iris.txt           symnmf_iris.txt
+rm sym_drive1.txt                   ddg_drive1.txt          norm_drive1.txt         symnmf_drive1.txt
+rm sym_drive2.txt                   ddg_drive2.txt          norm_drive2.txt         symnmf_drive2.txt
